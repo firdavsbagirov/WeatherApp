eca455b1ef6b159515159aa8604ea57cca4c3fea
Firdavs Bagirov <firdavsbagirov@gmail.com>
2020-11-01 20:50:50 +0500

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Firdavs Bagirov on 11/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
